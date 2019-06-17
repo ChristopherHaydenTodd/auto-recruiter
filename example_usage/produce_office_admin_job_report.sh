f0f0f80424e56392bbff1514bfcd1932c8b0f459
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-06-17 17:29:36 -0400

    adding and exmaple usage dir with a shell script calling the generate report with some normal configs to show an example of how to use the project

---
+++ example_usage/produce_office_admin_job_report.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+#
+# Prodcue "Office Admin" Job Report
+#
+# Example: shell produce_office_admin_job_report.sh
+#
+
+echo "$(date +%c): Generating 'Office Administrator' Job Report"
+python3.6 ../auto_recruiter/generate_job_report.py --report-output-filename="office_admin_jobs" \
+--report-output-dir="../data/job_reports" --min-jobs=15 --job-boards="indeed" \
+--job-boards="monster" --job-title="Administrative Assistant"\
+ --job-title="Office Administrator" --job-title="Office Assistant" \
+ --job-title="Meeting Coordinator"
