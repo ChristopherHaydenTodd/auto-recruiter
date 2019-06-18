5710871b59fbe50ab1757b6495b167a90c5b979f
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-06-18 10:18:49 -0400

    updating to 200 jobs per title

--- example_usage/produce_office_admin_job_report.sh
+++ example_usage/produce_office_admin_job_report.sh
@@ -7,7 +7,7 @@
 
 echo "$(date +%c): Generating 'Office Administrator' Job Report"
 python3.6 ../auto_recruiter/generate_job_report.py --report-output-filename="office_admin_jobs" \
---report-output-dir="../data/job_reports" --min-jobs=15 --job-boards="indeed" \
+--report-output-dir="../data/job_reports" --min-jobs=200 --job-boards="indeed" \
 --job-boards="monster" --job-title="Administrative Assistant"\
  --job-title="Office Administrator" --job-title="Office Assistant" \
  --job-title="Meeting Coordinator"
